e611d5220e03e5afd69ad9c6d7ca05bbef5b2a0d
shambijam <shambiflex@gmail.com>
2025-08-30 05:58:29 +0200

    ajout_fonction_trailing_stop

--- reporter.py
+++ reporter.py
@@ -22,10 +22,8 @@ import pandas as pd
 
 from phase_observer.detectors import Detectors
 
-<<<<<<< HEAD
-=======
 
->>>>>>> f854050 (ajout_fonction_trailing_stop)
+
 # ---------- Data Records (structures claires pour JSONL) ----------
 
 
@@ -112,11 +110,6 @@ class PhaseObserverReporter:
     """
 
     def __init__(self, config_manager=None, logger=None):
-<<<<<<< HEAD
-     
-=======
->>>>>>> f854050 (ajout_fonction_trailing_stop)
-
         self.logger = logger or logging.getLogger(__name__)
         self.config_manager = config_manager
         self.detectors = Detectors(logger=self.logger, config_manager=config_manager)
