11acb2e9548aac85faa6a28219350f4d1bdd7235
shambijam <shambiflex@gmail.com>
2025-09-23 10:43:38 +0200

    adaptation_strategies_fixe_selon _actifs_7

--- core/decision_pipeline.py
+++ core/decision_pipeline.py
@@ -284,11 +284,11 @@ class DecisionPipeline:
                     "liquidity": {
                         "EURUSD": {
                             # signature: (config_manager, strategy_config=None, logger=None, asset=None)
-                            "instance": LiquidityStrategy(self.config_manager, sca_cfg, sca_logger),
+                            "instance": LiquidityStrategy(self.config_manager, sca_cfg, liq_logger),
                             "strategy_name": "liquidity",
                         },
                         "GBPUSD": {
-                            "instance": LiquidityStrategy(self.config_manager, sca_cfg, sca_logger),
+                            "instance": LiquidityStrategy(self.config_manager, sca_cfg, liq_logger),
                             "strategy_name": "liquidity",
                         },
                     },
