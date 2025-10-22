f348aa4ecee46dd8c7fcd70c36c674ce782b6ae9
shambijam <shambiflex@gmail.com>
2025-10-22 11:53:21 +0200

    refacto_burst_SLTP_1

--- trader/trade_executor.py
+++ trader/trade_executor.py
@@ -14,8 +14,7 @@ except Exception:  # MT5 peut ne pas être dispo en environnement de test
     mt5 = None  # noqa
 
 from core.utils import CustomJSONEncoder
-from trader.errors import TradeExecutionError, InvalidDecisionPackageError
-
+from trader.errors import TradeExecutionError
 # --- Briques (binding en bas du fichier) ---
 from trader.order_builder import prepare_order, _build_mt5_request
 from trader.sizing import _calculate_risk_based_volume
