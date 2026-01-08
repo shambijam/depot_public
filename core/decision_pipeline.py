1a1e8c752ca0adaa169616e189d5950c16115ca5
shambijam <shambiflex@gmail.com>
2026-01-08 20:35:13 +0100

    Maj_scalping_memory_price _10

--- core/decision_pipeline.py
+++ core/decision_pipeline.py
@@ -70,7 +70,7 @@ class DecisionPipeline:
 
         # ✅ Cache local des configs assets
         self.asset_configs: Dict[str, Dict[str, Any]] = {}
-        for asset in ["EURUSD", "GBPUSD", "USDJPY"]:
+        for asset in ["NAS100", "GBPUSD", "USDJPY"]:
             try:
                 cfg = self.config_manager.load_asset_config(asset)
                 self.asset_configs[asset] = cfg
